0a6d218d8f0d5cec4e60e801e241c26bb273a50d
mgks <hello@mgks.dev>
2025-02-02 15:07:19 +0530

    service workers update

---
+++ sw.js
@@ -0,0 +1,82 @@
+const CACHE_NAME = 'toolbox-cache-v1';
+const urlsToCache = [
+  // Internal Libraries
+  '/',
+  '/index.html',
+  '/src/style.css',
+  '/src/script.js',
+
+  // Tool Scripts
+  '/tools/jekyll-post-generator/index.html',
+  '/tools/scripts/jekyll-post-generator.js',
+
+  // External Libraries
+  'https://unpkg.com/turndown/dist/turndown.js',
+  'https://unpkg.com/turndown-plugin-gfm/dist/turndown-plugin-gfm.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/showdown/2.1.0/showdown.min.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          (response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
